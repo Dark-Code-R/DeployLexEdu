a5c2b8a8882fdd5f1877a8c32f44b9f6f7115306
Rodrigo <rodrigo.darkcode@gmail.com>
2024-06-17 23:14:09 -0700

    Actualizar final

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def get_chat_response():
         return jsonify({'response': f"Error en la conexión con el servidor de Groq: {e}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
